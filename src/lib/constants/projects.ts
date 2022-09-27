b30ba9c073ee6ddd05f0aff1fc8bc6882865413c
Hamza <hamzakv333@gmail.com>
2022-09-27 08:21:10 -0400

    description update

--- src/lib/constants/projects.ts
+++ src/lib/constants/projects.ts
@@ -3,49 +3,49 @@ import images from 'src/lib/constants/images';
 const projects = {
     'qubic-labs': {
         name: 'Qubic Labs',
-        description: 'Q Labs is a research and development company that focuses on the development of quantum computing technologies. I worked on the development of a quantum simulator that is used to test quantum algorithms and quantum circuits.',
+        description: 'Qubic Labs is a startup dedicated in using wearable technology to improve physical fitness.',
         skills: ['C#', 'C', 'C++', 'EagleCAD'],
         images: [],
         links: []
     },
     'st-michaels': {
         name: 'St. Michael\'s Hospital',
-        description: 'St. Michael\'s College School is a private school in Toronto, Ontario. I worked on the development of a web application that is used to manage the school\'s student information system.',
+        description: 'Worked with Dr. Flora Matheson, Scientist at St. Michel’s Hospital, to develop a multi-platform mobile personal therapy app using React Native. Dr. Matheson was one of the first researchers in Canada to investigate and measure the strong relationship between problem gambling and homelessness. She and Good Shepherd Ministries co-developed and launched the first gambling-addiction program for people who are homeless.',
         skills: ['JavaScript', 'React Native'],
         images: [],
         links: []
     },
     'predict-technologies': {
         name: 'Predict Technologies',
-        description: 'Predict Technologies is a company that focuses on the development of a predictive analytics platform. I worked on the development of a web application that is used to manage the company\'s predictive analytics platform.',
+        description: 'Predict Technologies is a company that provides Patient Health Integrated Electronic Records (PHIER). PHIER is designed to assist and improve the diagnostic process for practicing physicians who currently employ electronic health record systems.',
         skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'Express', 'Postgres', 'AWS'],
         images: [],
         links: []
     },
     'islamic-institute-of-toronto': {
         name: 'Islamic Institute of Toronto',
-        description: 'The Islamic Institute of Toronto is a non-profit organization that focuses on the development of Islamic education. I worked on the development of a web application that is used to manage the organization\'s student information system.',
+        description: 'The Islamic Institute of Toronto is a premier Islamic Educational Institution, located in Toronto Canada offering post-secondary courses in Arabic Language, Islamic Jurisprudence, Ethics and Spirituality, Islamic History and Civilization amongst others.',
         skills: ['HTML', 'CSS', 'JavaScript', 'WordPress'],
         images: [images.iitWeb],
         links: []
     },
     'hiride': {
         name: 'HiRide',
-        description: 'HiRide is a company that focuses on the development of a ride-sharing platform. I worked on the development of a web application that is used to manage the company\'s ride-sharing platform.',
+        description: 'HiRide is a carpooling app for long distance travel, with the mission to improve the interaction between riders and drivers',
         skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'Express', 'Postgres', 'Docker', 'Kubernetes', 'AWS'],
         images: [images.hirideWeb],
         links: []
     },
     'techguilds': {
         name: 'TechGuilds',
-        description: 'TechGuilds is a company that focuses on the development of a web application that is used to manage the company\'s student information system.',
+        description: 'TechGuilds is a proudly Canadian company that specializes in digital customer experiences, the company\'s heritage has been with Sitecore, and envisions to include SaaS-based, headless CMS and microservice architecture. Some of the projects developed are: Peek WebXR, a seamless VR/AR experience plug-in for Sitecore and Kajoo, an AI-powered no/low-code solution that converts design mock-ups to production-ready code.',
         skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'Express', 'Postgres', 'Azure'],
         images: [images.techguildsWeb],
         links: []
     },
     'realme': {
         name: 'Realme',
-        description: 'Realme is a company that focuses on the development of a web application that is used to manage the company\'s student information system.',
+        description: 'RealMe is a platform that provides micro-investors access to real estate investments.',
         skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Remix', 'Node.js', 'Express', 'Postgres', 'Docker', 'Kubernetes', 'AWS', 'Polygon', 'Solidity'],
         images: [images.realmeWeb],
         links: []
